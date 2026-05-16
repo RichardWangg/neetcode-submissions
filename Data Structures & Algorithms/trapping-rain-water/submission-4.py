35e3b804a5a8070a1e049c8b153d9eeadf8546b1
RichardWangg <109177702+RichardWangg@users.noreply.github.com>
2026-05-16 16:57:26 -0400

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.py
@@ -0,0 +1,41 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) < 3:
+            return 0
+            
+        maxIdx = 0
+        for i in range(len(height)):
+            if height[i] > height[maxIdx]:
+                maxIdx = i
+
+        area = 0
+        # left to right pass up to highest point
+        l, r = 0, 1
+        while (r < len(height) and r <= maxIdx):
+            if height[l] > height[r]:
+                r += 1
+                continue
+            # closed area --> calculate area
+            maxHeight = min(height[l], height[r])
+            for i in range(l + 1, r):
+                h = maxHeight - height[i]
+                w = 1
+                area += h*w
+
+            l, r = r, r + 1
+        # right to left pass up to highest point
+        l, r = len(height) - 2, len(height) - 1
+        while (l > -1 and l >= maxIdx):
+            if height[r] > height[l]:
+                l -= 1
+                continue
+            # closed area --> calculate area
+            maxHeight = min(height[l], height[r])
+            for i in range(l + 1, r):
+                h = maxHeight - height[i]
+                w = 1
+                area += h*w
+            
+            r, l = l, l - 1
+
+        return area
