3dc4f952864676d17a6700a72b201faac22f59f6
RichardWangg <109177702+RichardWangg@users.noreply.github.com>
2026-05-17 00:54:07 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        vectors = [(position[i], speed[i]) for i in range(n)]
+        vectors = sorted(vectors, key = lambda v: v[0])
+        stack = [vectors[n - 1]] # stack (pos, speed)
+        print(vectors)
+
+        for i in range(n - 2, -1, -1):
+            # Check if it can merge with fleet directly ahead of it, if not its a new fleet
+            fleetPos, fleetSpeed = stack[-1]
+            currPos, currSpeed = vectors[i]
+            numer = currPos - fleetPos
+            denom = fleetSpeed - currSpeed
+            print(f"numer: {numer}, denom: {denom}")
+            
+            interceptTime = -1 if denom == 0 else numer / denom
+            interceptPos = currSpeed*interceptTime + currPos
+            fleetTargetTime = (target - fleetPos) / fleetSpeed
+            print(f"intercept time: {interceptTime}, inter pos: {interceptPos}, fleet target time: {fleetTargetTime}")
+            isMerge = interceptPos <= target and interceptTime <= fleetTargetTime and interceptTime > 0
+
+            if not isMerge:
+                stack.append((currPos, currSpeed)) # new fleet
+        
+        return len(stack)
